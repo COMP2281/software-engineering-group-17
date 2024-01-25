331250ce87606427645d1e9e4800b48fc2cfceee
JoshisonGitHub <joshua.d.pulham@durham.ac.uk>
2024-01-25 23:56:21 +0000

    Add question to overworld

---
+++ game/SEProject/Assets/Scripts/s_button.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class s_button : MonoBehaviour
+{
+    public S_player_movement player;
+    private bool in_range;
+
+    public bool correct_answer;
+
+    public GameObject correct_answerbox;
+    public GameObject wrong_answerbox;
+
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && in_range)
+        {
+            if(correct_answer)
+            {
+                if (correct_answerbox.activeInHierarchy)
+                {
+                    correct_answerbox.SetActive(false);
+                }
+                else
+                {
+                    correct_answerbox.SetActive(true);
+                }
+                StartCoroutine(OpenDoor());
+                player.canmove = false;
+                wall.SetActive(false);
+            }
+            else {
+                if (wrong_answerbox.activeInHierarchy)
+                {
+                    wrong_answerbox.SetActive(false);
+                }
+                else
+                {
+                    wrong_answerbox.SetActive(true);
+                }
+                StartCoroutine(Restart());
+                player.canmove = false;
+            }
+
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            in_range = true;
+        }
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    IEnumerator OpenDoor()
+    {
+        yield return new WaitForSeconds(2);
+        correct_answerbox.SetActive(false);
+        player.canmove = true;
+    }
+}
---
+++ game/SEProject/Assets/Scripts/s_sign.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class s_sign : MonoBehaviour
+{
+    public GameObject question_box;
+    private bool in_range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && in_range)
+        {
+            //Debug.Log("works");
+            if(question_box.activeInHierarchy)
+            {
+                question_box.SetActive(false);
+            }
+            else
+            {
+                question_box.SetActive(true);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            in_range = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            in_range = false;
+            question_box.SetActive(false);
+        }
+    }
+}
