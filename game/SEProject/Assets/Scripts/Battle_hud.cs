db7d02f59bc950aa083c610c3899e8b6eb364dca
Shee69 <lehoangnamtep@gmail.com>
2024-02-10 18:46:52 +0000

    Add more changes to combat turn

--- game/SEProject/Assets/Scripts/Battle_hud.cs
+++ game/SEProject/Assets/Scripts/Battle_hud.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Battle_hud : MonoBehaviour
+public class Battle_Hud : MonoBehaviour
 {
     public Text name_text;
     public int current_hp;
     public int max_hp;
 
-    public void Set_hud(Combat_unit unit)
+    public void Set_hud(Combat_Unit unit)
     {
         name_text.text = unit.unit_name;
         current_hp = unit.current_hp;
--- game/SEProject/Assets/Scripts/Battle_system.cs
+++ game/SEProject/Assets/Scripts/Battle_system.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum Battle_state { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public enum Battle_State { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class Battle_system : MonoBehaviour
 {
@@ -14,24 +14,24 @@ public class Battle_system : MonoBehaviour
     public Transform player_battlestation;
     public Transform enemy_battlestation;
 
-    Combat_unit player_unit;
-    Combat_unit enemy_unit;
+    Combat_Unit player_unit;
+    Combat_Unit enemy_unit;
 
     public Text dialogue;
 
-    public Battle_hud player_hud;
-    public Battle_hud enemy_hud;
+    public Battle_Hud player_hud;
+    public Battle_Hud enemy_hud;
 
-    public Battle_state state;
+    public Battle_State state;
 
     // Start is called before the first frame update
     void Start()
     {
-        state = Battle_state.START;
-        StartCoroutine(SetupBattle());
+        state = Battle_State.START;
+        StartCoroutine(Setup_Battle());
     }
 
-    IEnumerator SetupBattle()
+    IEnumerator Setup_Battle()
     {
         GameObject player_go = Instantiate(player_prehab, player_battlestation);
         player_unit = player_go.GetComponent<Combat_unit>();
@@ -46,12 +46,50 @@ public class Battle_system : MonoBehaviour
 
         yield return new WaitForSeconds(2f);
 
-        state = Battle_state.PLAYERTURN;
-        Player_turn();
+        state = Battle_State.PLAYERTURN;
+        Player_Turn();
     }
 
-    void Player_turn()
+    void Player_Turn()
     {
         dialogue.text = "User action User action User action User action";
+    }
+
+    IEnumerator Player_Attack()
+    {
+        bool is_dead = enemy_unit.Take_damage(player_unit.damage);
+        enemy_hud.Set_hp(enemy_unit.current_hp);
+        dialogue.text = "The enemy took damage";
+
+        yield return new WaitForSeconds(2f);
+
+        if (is_dead)
+        {
+            state = Battle_State.WON;
+            End_Battle();
+        } else
+        {
+            state = Battle_State.ENEMYTURN;
+            StartCoroutine(Enemy_Turn());
+        }
+    }
+
+    void End_Battle()
+    {
+        if (state == Battle_State.WON)
+        {
+            dialogue.text = "You won!";
+        } else if (state == Battle_State.LOST)
+        {
+            dialogue.text = "You lost!";
+        }
+    }
+
+    IEnumerator Enemy_Turn()
+    {
+        dialogue.text = "Question";
+
+        yield return new WaitForSeconds(1f);
+
     }
 }
