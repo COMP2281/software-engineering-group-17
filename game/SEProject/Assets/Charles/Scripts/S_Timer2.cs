8280b7659979169cff3f28ba8c21576b4783c304
cdv123 <115459644+cdv123@users.noreply.github.com>
2024-02-17 22:54:48 +0000

    Finish layout and turret placement of puzzle 3 (#87)

---
+++ game/SEProject/Assets/Charles/Scripts/S_Timer2.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class S_Timer2 : MonoBehaviour
+{
+    public S_player_movement_2 player;
+
+    public float targetTime = 60.0f;
+
+    public AudioSource _audioSource;
+
+    public AudioSource yay;
+
+    public AudioSource musicSource;
+
+    private bool startTimer = false;
+
+    public Text timeText;
+
+    public BoxCollider2D box;
+
+    public bool stoptimer = false;
+
+    private bool overflowstopper = true;
+
+    private bool overflowstopper2 = true;
+
+    private void Start()
+    {
+        //_audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        
+        if (!stoptimer)
+        {
+            if (startTimer)
+            {
+                //manager.instance.StopMusic();
+                if (overflowstopper2)
+                {
+                    overflowstopper2 = false;
+                    musicSource.Play();
+                }
+                targetTime -= Time.deltaTime;
+                updateTimer(targetTime);
+                if (targetTime <= 0.0f)
+                {
+                    targetTime = 0.0f;
+                    if(overflowstopper)
+                    {
+                        overflowstopper = false;
+                        timerEnded();
+                    }
+                    
+                }
+            }
+        }
+        else
+        {
+           
+            
+            if(overflowstopper)
+            {
+                overflowstopper = false;
+                musicSource.Stop();
+                
+                yay.Play();
+                StartCoroutine(PlayMusic());
+            }
+
+            
+        }
+    }
+    
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+
+    void timerEnded()
+    {
+        player.canmove = false;
+        _audioSource.Play();
+        player.canmove = false;
+        StartCoroutine(Restart());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            startTimer = true;
+            box.enabled = false;
+            Debug.Log("start");
+        }
+    }
+
+    IEnumerator Restart()
+    {
+        
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    IEnumerator PlayMusic()
+    {
+
+        yield return new WaitForSeconds(3);
+    }
+}
+
