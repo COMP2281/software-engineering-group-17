6d9354f7cf7168bd42ead6f03c054c98beaff433
cdv123 <115459644+cdv123@users.noreply.github.com>
2024-02-22 13:38:15 +0000

    Change portal class and script name (#115)

--- game/SEProject/Assets/Charles/Scripts/AI_Portal.cs
+++ game/SEProject/Assets/Charles/Scripts/AI_Portal.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class AI_Portal : MonoBehaviour
 {
     public int index;
     private GameObject player;
