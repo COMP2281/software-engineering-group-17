aafe9ff82de9977b96b5a7fbcdb39c66e2798e89
JoshisonGitHub <joshua.d.pulham@durham.ac.uk>
2024-02-22 19:13:40 +0000

    Add combat sprites

--- game/SEProject/Assets/Nam/Scripts/PlayerTurn.cs
+++ game/SEProject/Assets/Nam/Scripts/PlayerTurn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerTurn : State
 {
@@ -22,15 +23,27 @@ public class PlayerTurn : State
             isEnemyDead = BattleSystem.enemyUnit.TakeDamage(BattleSystem.playerUnit.damage);
             BattleSystem.enemyHud.SetHp(BattleSystem.enemyUnit.currentHp);
             BattleSystem.dialogue.text = "Correct, " + BattleSystem.enemyUnit.unitName + " took " + BattleSystem.playerUnit.damage + " HP";
+            
+            BattleSystem.playerPrefab.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            BattleSystem.playerPrefab.gameObject.transform.GetChild(2).gameObject.SetActive(false);
+
         } 
         else
         {
             isPlayerDead = BattleSystem.playerUnit.TakeDamage(BattleSystem.enemyUnit.damage);
             BattleSystem.playerHud.SetHp(BattleSystem.playerUnit.currentHp);
             BattleSystem.dialogue.text = "Incorrect, you lost " + BattleSystem.enemyUnit.damage + " HP";
+
+            BattleSystem.playerPrefab.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            BattleSystem.playerPrefab.gameObject.transform.GetChild(2).gameObject.SetActive(false);
         }
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(5);
+        for (int i = 0; i < 2; i++)
+        {
+            BattleSystem.playerPrefab.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        BattleSystem.playerPrefab.gameObject.transform.GetChild(2).gameObject.SetActive(true);
 
         if (isEnemyDead)
         {
