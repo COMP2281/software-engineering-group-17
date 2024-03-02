edf54219a1afaecc66ea5a750b4c182c16d48fdf
Shee69 <lehoangnamtep@gmail.com>
2024-03-02 21:59:05 +0000

    Remove unecessary variable in combat unit

--- game/SEProject/Assets/Nam/Scripts/BattleHud.cs
+++ game/SEProject/Assets/Nam/Scripts/BattleHud.cs
@@ -8,15 +8,12 @@ public class BattleHud : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI hpText;
     public int currentHp;
-    public int maxHp;
 
     public void SetHud(CombatUnit unit)
     {
         nameText.text = unit.unitName;
         currentHp = unit.currentHp;
         hpText.text = unit.currentHp.ToString();
-        maxHp = unit.maxHp;
-
     }
     public void SetHp(int hp)
     {
