05e5658d761af5dcc226679f09675bace8946c46
Josh <69389855+JoshisonGitHub@users.noreply.github.com>
2024-03-04 11:33:59 +0000

    Add second co-worker to hub world + Add AI world levels to build (#159)

--- game/SEProject/Assets/Josh/Scripts/Hub World/CoWorkerSpawner.cs
+++ game/SEProject/Assets/Josh/Scripts/Hub World/CoWorkerSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CoWorkerSpawner : MonoBehaviour
 {
     public GameObject coWorker1;
-    //public GameObject coWorker2;
+    public GameObject coWorker2;
     //public GameObject coWorker3;
     //public GameObject coWorker4;
     // Start is called before the first frame update
@@ -16,6 +16,10 @@ public class CoWorkerSpawner : MonoBehaviour
         if (GM.gmInstance.GetItem1())
         {
             coWorker1.SetActive(true);
+        }
+        if (GM.gmInstance.GetItem2())
+        {
+            coWorker2.SetActive(true);
         }
     }
 }
